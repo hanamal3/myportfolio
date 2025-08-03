58144c6e0c62d81d6322479833852a951e81ad9c
hanamal3 <info.malmal3@gmail.com>
2025-08-03 13:30:29 +0900

    fix: unoptimized: true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   images: {
     domains: ['images.microcms-assets.io'],
+    unoptimized: true, // ← これを追加
   },
   output: 'export', // Next.js 13以降の設定例
   basePath: '/myportfolio',
