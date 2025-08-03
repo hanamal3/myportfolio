1b5f9add2ab68a17ef100bf383a95a6927bb43a4
hanamal3 <info.malmal3@gmail.com>
2025-08-03 12:53:27 +0900

    デプロイの設定追加

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ['images.microcms-assets.io'],
   },
+  output: 'export', // Next.js 13以降の設定例
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
 };
