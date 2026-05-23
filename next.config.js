80a5b31e826183304eda105eb2170027a8b4dcc2
hanamal3 <info.malmal3@gmail.com>
2026-05-23 19:15:40 +0900

    Next→indexに修正

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['images.microcms-assets.io'],
-    unoptimized: true, // ← これを追加
-  },
-  output: 'export', // Next.js 13以降の設定例
-  basePath: '/myportfolio',
-  assetPrefix: '/myportfolio/',
-};
--- src/libs/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || '', // 例: 'my-portfolio'
-  apiKey: process.env.MICROCMS_API_KEY || '',               // microCMSのAPIキー
-});
